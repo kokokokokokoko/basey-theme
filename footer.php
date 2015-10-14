cc4c0512be5f3a4829602b875be0c94fd50b6664
James <pinecone@pak0.com>
2015-10-14 15:13:11 -0700

    Added footer

--- footer.php
+++ footer.php
@@ -1,9 +1,16 @@
-<?php
-
-do_action( 'basey_footer' );
-wp_footer();
-//do_action( 'basey_debug' );
-
-?>
+<div class="uk-block uk-block-secondary uk-contrast uk-margin-large-top">
+	<div class="uk-container">
+		<?php
+		do_action( 'basey_footer' );
+		wp_footer();
+		//do_action( 'basey_debug' );
+		?>
+		<div class="uk-width-1-1">
+			<p class="uk-text-center bottom-txt">
+				Copyright <?php echo date(Y); ?> <?php bloginfo('name'); ?>
+			</p>
+		</div>
+	</div>
+</div>
 	</body>
 </html>
